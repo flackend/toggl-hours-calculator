5bd23d9f18309aaf047fcf0797bce38ff02120fd
Jared Flack <jared.flack@gmail.com>
2013-11-06 18:46:59 -0500

    Fixed script.js (last commit was accidentally an a preliminary version of the script)

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-var totalMinutes = 0;
-$('.task input:checked').each(function() {
+;(function () {
 
-    var text = $(this).parent().find('.duration').text();
-    var arr = text.split(':');
-    var minutes = parseInt(arr[arr.length - 2]);
-    var seconds = parseInt(arr[arr.length - 1]);
+    "use strict";
 
-    totalMinutes += (seconds / 60) + minutes;
-});
+    var totalHours = 0;
+    $(".task input:checked").each(function () {
 
-var totalHours = Math.round(totalMinutes / 60 * 100) / 100;
-alert('Total hours of checked items: '+ totalHours);
+        // Get Duration
+        var durationStr = $(this).parent().find(".duration").text();
+        var duration = durationStr.split(":");
+        // Parse duration
+        var hours = 0;
+        if (duration[duration.length - 3] !== undefined) {
+            hours = parseInt(duration[duration.length - 3], 10);
+        }
+        var minutes = parseInt(duration[duration.length - 2], 10);
+        var seconds = parseInt(duration[duration.length - 1], 10);
+        // Calculate hours
+        totalHours += (seconds / 60 + minutes) / 60 + hours;
+    });
+    // Round hours (2.68781 becomes 2.69)
+    totalHours = Math.round(totalHours * 100) / 100;
+    // Display value (using a prompt so that the user can easily copy the
+    // value to their clipboard)
+    var foo = prompt("Total hours of checked items:", totalHours);
+
+})();
